233e6b3f591c6833804c33c3114bf74ddad5f599
cahilfil <yunus.aydin@protonmail.com>
2025-02-09 05:12:08 -0800

    flake and actions

--- flake.nix
+++ flake.nix
@@ -1,32 +1,45 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
     forester.url = "sourcehut:~jonsterling/ocaml-forester";
+    forester.inputs.nixpkgs.follows = "nixpkgs";
+    theme.url = "github:cahilfil/forest-theme?ref=main";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, flake-utils, forester }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
+  outputs = inputs@{ self, forester, theme, nixpkgs }:
+    let
+      pkgs = import nixpkgs { inherit system inputs; };
+      system = "x86_64-linux";
     in {
-      packages.default = pkgs.stdenv.mkDerivation {
-        name = "forest-website";
-        src = ./.;
-        
-        nativeBuildInputs = [
-          forester.packages.${system}.default
-          pkgs.nodePackages.katex  # Required by forester
-        ];
+      devShells.${system}.default =
+        pkgs.mkShell { buildInputs = [ forester.packages.${system}.default ]; };
 
+      packages.${system}.site = pkgs.stdenv.mkDerivation {
+        name = "my-static-site";
+        src = pkgs.runCommand "merged-src" { } ''
+          mkdir -p $out
+          echo "DEBUG: Listing contents of local source (./.):"
+          ls -la ${./.}
+
+          echo "Copying local source contents to \$out"
+          cp -r ${./.}/* $out || true
+
+          echo "DEBUG: Listing contents of theme folder:"
+          ls -la ${theme.packages.${system}.theme} || true
+
+          echo "Copying theme contents to \$out"
+          mkdir $out/theme
+          cp -r ${theme.packages.${system}.theme}/* $out/theme/ || true
+        '';
+
+        buildInputs = [ forester.packages.${system}.default ];
         buildPhase = ''
-          export HOME=$TMPDIR  # Clean environment
           forester build forest.toml
         '';
-        
         installPhase = ''
-          mkdir -p $out
-          cp -r output/* $out/
+          cp -r output $out
         '';
       };
-    });
+    };
 }
+
